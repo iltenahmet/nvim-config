fe2590578cf57aed7b1447de6536d06e0461d638
iltenahmet <iltenahmet@gmail.com>
2023-12-17 16:18:39 -0500

    add debugger

--- init.lua
+++ init.lua
@@ -222,6 +222,7 @@ require('lazy').setup({
   --       Uncomment any of the lines below to enable them.
   -- require 'kickstart.plugins.autoformat',
   -- require 'kickstart.plugins.debug',
+  require 'plugins.debug'
 
   -- NOTE: The import below can automatically add your own plugins, configuration, etc from `lua/custom/plugins/*.lua`
   --    You can use this folder to prevent any conflicts with this init.lua if you're interested in keeping
