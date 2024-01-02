4578c33668337459c7990283b0e5d6a1731158ae
iltenahmet <iltenahmet@gmail.com>
2024-01-02 16:16:44 +0300

    debugger config

--- lua/custom/plugins/debug.lua
+++ lua/custom/plugins/debug.lua
@@ -84,5 +84,37 @@ return {
 
     -- Install golang specific config
     require('dap-go').setup()
+
+    --if not vim.fn.has('macunix') then
+      dap.adapters.lldb = {
+        type = 'executable',
+        command = 'C:\\Program Files\\LLVM\\bin\\lldb-vscode.exe',
+        name = 'lldb'
+      }
+    --end
+
+    dap.configurations.cpp = {
+      {
+        name = 'Launch',
+        type = 'lldb',
+        request = 'launch',
+        program = function()
+          return vim.fn.input('Path to executable: ', vim.fn.getcwd() .. '/', 'file')
+        end,
+        cwd = '${workspaceFolder}',
+        stopOnEntry = false,
+        args = {"< test.txt", " > out.txt"},
+
+        -- 💀
+        -- if you change `runInTerminal` to true, you might need to change the yama/ptrace_scope setting:
+        --    echo 0 | sudo tee /proc/sys/kernel/yama/ptrace_scope
+        -- Otherwise you might get the following error:
+        --    Error on launch: Failed to attach to the target process
+        -- But you should be aware of the implications:
+        -- https://www.kernel.org/doc/html/latest/admin-guide/LSM/Yama.html
+        -- runInTerminal = false,
+      },
+    }
+
   end,
 }
