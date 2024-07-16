6a2e3250a03a90a3e45261f28e5f74b948fa4c2f
iltenahmet <iltenahmet@gmail.com>
2024-07-16 10:56:51 -0400

    which-key update, colorscheme gruvbox

--- init.lua
+++ init.lua
@@ -73,7 +73,7 @@ require('lazy').setup({
   },
 
   -- Useful plugin to show you pending keybinds.
-  { 'folke/which-key.nvim', opts = {} },
+  { 'folke/which-key.nvim', opts = { icons = {mappings = false} } },
   {
     -- Adds git related signs to the gutter, as well as utilities for managing changes
     'lewis6991/gitsigns.nvim',
@@ -317,8 +317,8 @@ require('lazy').setup({
 
 --Ahmet - color scheme
   -- vim.o.background = "dark"
-  vim.cmd('colorscheme github_dark')
-  -- vim.cmd('colorscheme gruvbox')
+  -- vim.cmd('colorscheme github_dark')
+  vim.cmd('colorscheme gruvbox')
 
   -- disables automatic conversion of spaces to tabs when pressing the tab key
   -- vim.o.noexpandtab = true
@@ -614,24 +614,21 @@ local on_attach = function(_, bufnr)
   end, { desc = 'Format current buffer with LSP' })
 end
 
+-- { "<leader>", group = "VISUAL <leader>", mode = "v" },
 -- document existing key chains
-require('which-key').register {
-  ['<leader>c'] = { name = '[C]ode', _ = 'which_key_ignore' },
+require('which-key').add {
+  {'<leader>c', group = '[C]ode' },
   --['<leader>d'] = { name = '[D]ocument', _ = 'which_key_ignore' },
-  ['<leader>g'] = { name = '[G]it', _ = 'which_key_ignore' },
-  ['<leader>h'] = { name = 'Git [H]unk', _ = 'which_key_ignore' },
-  ['<leader>r'] = { name = '[R]ename', _ = 'which_key_ignore' },
-  ['<leader>s'] = { name = '[S]earch', _ = 'which_key_ignore' },
-  ['<leader>t'] = { name = '[T]oggle', _ = 'which_key_ignore' },
-  ['<leader>w'] = { name = '[W]orkspace', _ = 'which_key_ignore' },
+  {'<leader>g', group = '[G]it'},
+  {'<leader>h', group = 'Git [H]unk'},
+  {'<leader>r', group = '[R]ename'},
+  {'<leader>s', group = '[S]earch'},
+  {'<leader>t', group = '[T]oggle'},
+  {'<leader>w', group = '[W]orkspace'},
+  {'<leader>', group = 'VISUAL <leader>', mode = "v" },
+  {'<leader>h', group = 'Git [H]unk', mode = "v" }
 }
--- register which-key VISUAL mode
--- required for visual <leader>hs (hunk stage) to work
-require('which-key').register({
-  ['<leader>'] = { name = 'VISUAL <leader>' },
-  ['<leader>h'] = { 'Git [H]unk' },
-}, { mode = 'v' })
-
+--
 -- mason-lspconfig requires that these setup functions are called in this order
 -- before setting up the servers.
 require('mason').setup()
