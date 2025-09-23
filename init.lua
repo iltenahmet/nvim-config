a87cd446ace18a692614b6f1360a5a5f922ef80c
iltenahmet <iltenahmet@gmail.com>
2025-09-23 18:18:44 -0400

    .

--- init.lua
+++ init.lua
@@ -1051,7 +1051,7 @@ cmp.setup {
   },
 }
 
-vim.cmd.colorscheme("ayu-dark")
+vim.cmd.colorscheme("gruvbox")
 
 -- The line beneath this is called `modeline`. See `:help modeline`
 -- vim: ts=2 sts=2 sw=2 et
